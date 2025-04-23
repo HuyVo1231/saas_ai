a4477fcfc21baa9a7caed4387dd57504de4a8afd
HuyVo1231 <vonhathuy1306@gmail.com>
2025-04-23 13:43:16 +0700

    first commit, build layout and proteced routes.

---
+++ app/(landing)/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LandingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      LandingLayout
+      {children}
+    </div>
+  )
+}
+
+export default LandingLayout
