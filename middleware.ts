25ee75445132c67300e6335c23a258ad945627ad
HuyVo1231 <vonhathuy1306@gmail.com>
2025-05-02 14:46:53 +0700

    fix middleware 2

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,16 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/'])
+// const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/'])
 
-export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect()
-  }
-})
+// export default clerkMiddleware(async (auth, req) => {
+//   if (!isPublicRoute(req)) {
+//     await auth.protect()
+//   }
+// })
 
-export const config = {
-  matcher: [
-    // Áp dụng middleware cho các route tĩnh và dynamic, trừ file tĩnh
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)'
-  ]
-}
+// export const config = {
+//   matcher: [
+//     // Áp dụng middleware cho các route tĩnh và dynamic, trừ file tĩnh
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)'
+//   ]
+// }
