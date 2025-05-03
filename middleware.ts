88810f36cee236133b26d2e60cfc7eaa0c551dd8
HuyVo1231 <vonhathuy1306@gmail.com>
2025-05-03 08:19:30 +0700

    Fix all deploy, final

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,8 @@ const isPublicRoute = createRouteMatcher([
   '/',
   '/sign-in(.*)',
   '/sign-up(.*)',
-  '/api/webhook(.*)'
+  '/api/webhook(.*)',
+  '/api/stripe(.*)'
 ])
 
 export default clerkMiddleware((auth, req) => {
